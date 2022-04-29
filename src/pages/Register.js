c835c4c5700fc3bf6712044397b8594c954b3bd2
Fadi Zahhar <79freelancer@gmail.com>
2022-04-29 18:10:49 +0300

    adding the register form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
-import Logo from '../components/Logo';
 import Wrapper  from '../assets/wrappers/RegisterPage';
+import { Logo,FormRow } from '../components';
 
 const initialState = {
     name:'',
@@ -12,17 +12,72 @@ const Register = () => {
     const [values,setValues] = useState(initialState)
 
     const handleChange = (e) => {
-        console.log(e.traget);
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setValues({...values,[name]:value});
     }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(e.traget);
+        const {name, email, password, isMember} = values;
+        if(!email || !password || (!isMember && !name)) {
+            console.log("Please fill the required fields");
+        }
     }
 
+
+    const toogleMember = () => {
+        setValues({...values,isMember:!values.isMember})
+    }
   return (
     <Wrapper className='full-page'>
-        <form className='form' onSubmit={onSubmit}></form>
+        <form className='form' onSubmit={onSubmit}>
+            <Logo />
+            <h3>
+                {values.isMember ? 'Login' : 'Register' }
+            </h3>
+
+            {!values.isMember
+            &&
+            (<FormRow 
+            type="text"
+            name="name"
+            labelText="Name"
+            value={values.name}
+            handleChange={handleChange}
+            />)
+            }
+
+            {/* email field */}
+            <FormRow 
+            type="email"
+            name="email"
+            labelText="Email"
+            value={values.email}
+            handleChange={handleChange}
+            />
+
+
+            {/* password field */}
+            <FormRow 
+            type="password"
+            name="password"
+            labelText="Password"
+            value={values.password}
+            handleChange={handleChange}
+            />
+
+
+            <button type="submit" className="btn btn-block">
+                submit
+            </button>
+
+            <p>
+                {values.isMember ? 'Already a member' : 'Not a member'} 
+                <button type="button" onClick={toogleMember} className='member-btn'> {values.isMember ? 'Register' : 'Login' }</button>
+            </p>
+        </form>
     </Wrapper>
   )
 }

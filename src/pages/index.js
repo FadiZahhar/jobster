0c03b2bd4455849b4786133edb9088bcd9e14ed9
Fadi Zahhar <79freelancer@gmail.com>
2022-04-30 13:36:05 +0300

    fixing the error by removing the old dashboard component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Error from "./Error";
 import Landing from "./Landing";
 import Register from "./Register";
-import Dashboard from "./Dashboard";
 
 export {
     Error,
     Landing,
-    Register,
-    Dashboard
+    Register
 }

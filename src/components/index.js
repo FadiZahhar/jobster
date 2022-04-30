79b2b90313e3572ec18e1b8bead733493f68550d
Fadi Zahhar <79freelancer@gmail.com>
2022-04-30 13:44:37 +0300

    finishing the core of the dashboard layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import FormRow from "./FormRow";
 import Logo from "./Logo";
-
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
 export {
     Logo,
-    FormRow
+    FormRow,
+    Navbar,
+    SmallSidebar,
+    BigSidebar
 }
